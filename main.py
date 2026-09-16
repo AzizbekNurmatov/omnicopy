7bea7482a8194ecb1dd303c8ff08f9b67e4cd543
Azizbek Nurmatov <azizbeknurmatov15@gmail.com>
2026-09-16 16:49:20 -0400

    Interacting with the API

---
+++ main.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from random import randint
+from typing import Any
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI(root_path="/api/v1")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"} 
+
+data : Any = [
+    {"campaign_id": 1,
+     "name": "Summer Launch",
+     "due_date": datetime.now(),
+     "created_at": datetime.now(),
+     },
+    {"campaign_id": 2,
+     "name": "Black Friday",
+     "due_date": datetime.now(),
+     "created_at": datetime.now(),
+     }
+]
+
+@app.get("/campaigns")
+async def read_campaigns():
+    return {"campaigns": data}
+
+@app.get("/campaigns/{id}")
+async def read_campaign(id: int):
+    for campaign in data:
+        if campaign.get("campaign_id") == id:
+            return {"campaign": campaign}
+    raise HTTPException(status_code=404)
+
+@app.post("/campaigns")
+async def create_campaign(body: dict[str, Any]):
+    
+    new : Any = {
+        "campaign_id": randint(100, 1000),
+        "name": body.get("name"),
+        "due_date": body.get("due_date"),
+        "created_at": datetime.now()
+    }
+    
+    data.append(new)
+    return {"campaign": new}
