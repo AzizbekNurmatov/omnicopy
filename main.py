d32f1354f5ea431a24ef31dbb323bfbe7f8f01ce
Azizbek Nurmatov <azizbeknurmatov15@gmail.com>
2026-09-17 01:37:14 -0400

    building the type

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from contextlib import asynccontextmanager
-from datetime import datetime
+from datetime import datetime, timezone
+from email.policy import default
 from random import randint
 from typing import Annotated, Any
 from fastapi import Depends, FastAPI, HTTPException, Response
 from sqlalchemy import create_engine, select
-from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel import Field, SQLModel, Session, create_engine
+
+class Campaign(SQLModel, table=True):
+    campaign_id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    due_date: datetime | None = Field(default=None, index=True)
+    created_at: datetime = Field(default=lambda: datetime.now(timezone.utc), nullable=True, index=True)
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -24,8 +31,8 @@ SessionDep = Annotated[Session, Depends(get_session)]
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     create_db_and_tables()
-    #withSession(engine) as session:
-        #if not session.exec(select())
+    with Session(engine) as session:
+        if not session.exec(select())
     yield
 
 app = FastAPI(root_path="/api/v1", lifespan=lifespan)
